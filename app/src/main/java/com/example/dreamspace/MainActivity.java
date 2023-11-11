f746826d7225988c2e22a2aa55a11c833999a77d
bybLik04 <106867943+bybLik04@users.noreply.github.com>
2023-11-11 22:29:06 +0300

    сделал переходы в навбаре, дизайн на фраментах базу, иконку приложения

--- app/src/main/java/com/example/dreamspace/MainActivity.java
+++ app/src/main/java/com/example/dreamspace/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.dreamspace;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.dreamspace.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setNewFragment(new HomeFragment());
+
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.page_home) {
+                setNewFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.page_journal) {
+                setNewFragment(new JournalFragment());
+            } else if (item.getItemId() == R.id.page_setings) {
+                setNewFragment(new SettingsFragment());
+            }
+            return true;
+        });
+    }
+
+    private void setNewFragment(Fragment fragment) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frame_layout, fragment);
+        ft.addToBackStack(null);
+        ft.commit();
     }
 }
